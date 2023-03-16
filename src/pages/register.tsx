930f7a1f2728569557542f2c87afa977c9f91985
rayaanoidPrime <rayaanghosh18@gmail.com>
2023-03-16 12:19:13 +0530

    chakra integration with react hook form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form"
+import {
+    FormErrorMessage,
+    FormLabel,
+    FormControl,
+    Input,
+    Button,
+} from '@chakra-ui/react'
 import Wrapper from '../components/wrapper'
 
 
@@ -14,20 +21,30 @@ type registerProps = {
 
 const Register: React.FC<registerProps> = ({ }) => {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
 
     return (
         <Wrapper variant={'small'}>
             <form onSubmit={handleSubmit(onSubmit)}>
-                {/* register your input into the hook by invoking the "register" function */}
-                <input defaultValue="name" {...register("name", { required: true })} />
-
-                {/* errors will return when field validation fails  */}
-                {errors.name && <span>This field is required</span>}
-
-                <input type="submit" />
+                <FormControl isInvalid={!!errors.name}>
+                    <FormLabel htmlFor='name'>First name</FormLabel>
+                    <Input
+                        id='name'
+                        placeholder='name'
+                        {...register('name', {
+                            required: 'This is required',
+                            minLength: { value: 4, message: 'Minimum length should be 4' },
+                        })}
+                    />
+                    <FormErrorMessage>
+                        {errors.name && errors.name.message}
+                    </FormErrorMessage>
+                </FormControl>
+                <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                    Submit
+                </Button>
             </form>
         </Wrapper>
     );
